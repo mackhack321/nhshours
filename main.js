a5566afe818f57553c0c49aee92ec6f1d4ab386b
Mack Stanley <mlgprohacker@gmail.com>
2019-09-13 20:56:39 -0500

    begin work on mysql integration

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow } = require("electron");
+const sql = require('mysql');
 
 let win;
+
+function getInput() {
+    let date = document.getElementById("date").value;
+    let amt = document.getElementById("time").value;
+    let unit = document.getElementById("unit").value;
+    let desc = document.getElementById("desc").value;
+
+    if ( unit == "min" ) {
+        amt = ((amt / 60.0).toFixed(2)).toString();
+    }
+
+    var data = {date: date, amt: amt, desc: desc};
+    return data;
+}
+
+function buildQuery(data) {
+    var data = getInput();
+    console.log(data);
+    let query = "INSERT INTO hours (date, hours, description) VALUES ('" + data.date + "', '" + data.amt + "', '" + data.desc + "');";
+    console.log(query);
+}
 
 function createWindow() {
     win = new BrowserWindow({
