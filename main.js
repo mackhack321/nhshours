f99f6863437051c882a32c83f1813bdf4f055b8b
Mack Stanley <mlgprohacker@gmail.com>
2019-09-18 11:15:44 -0500

    make sample table, make sql function

--- main.js
+++ main.js
@@ -17,4 +17,30 @@ function getInput() {
 function buildQuery() {
     var data = getInput();
     let query = "INSERT INTO hours (date, hours, description) VALUES ('" + data.date + "', '" + data.amt + "', '" + data.desc + "');";
+    sendToDB(query);
+}
+
+function sendToDB(query) {
+    var con = sql.createConnection({
+        host: "10.0.0.235",
+        user: "code",
+        password: "Excision1519"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("established connection");
+    });
+}
+
+function updateTotal() {
+    var sum = 0.0;
+    var table = document.getElementById("datatable");
+    var totalhtml = document.getElementById("total");
+    for (var i = 1; i < table.rows.length; i++)
+    {
+        sum += parseFloat(table.rows[i].cells[1].innerHTML.split(" ")[0]);
+    }
+
+    totalhtml.innerHTML = "Total: " + sum + " hours<br>" + (50.0-sum) + " to go!";
 }
